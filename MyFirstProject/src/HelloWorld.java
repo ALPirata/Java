67347524cb3cf252678a4014bb1629bc6e156ae8
ALPirata <volodymyr.tryhub@gmail.com>
2017-01-03 13:58:43 +0200

    first upload

---
+++ MyFirstProject/src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world from inside Eclipse IDE!!!");
+		
+		System.out.println("Alex Jonsov");
+		System.out.println("23 Main Street");
+		System.out.println("New York, NY 10001");
+		System.out.println("USA");
+		System.out.println("sysout");
+
+	}
+
+}
---
+++ sale/src/com/sale/FriendsAndFamily.java
@@ -0,0 +1,20 @@
+package com.sale;
+
+import java.util.Scanner;
+
+public class FriendsAndFamily {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Your favorite store runs a 30% off sale on selected products.");
+		
+		Scanner input = new	Scanner(System.in);
+		
+		do {
+			
+			
+		} while (true);
+
+	}
+
+}
